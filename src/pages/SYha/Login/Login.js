bd6bc6f374808bd577ed2f35f13b3f87a6348876
seoyul <seoyul0203@gmail.com>
2022-08-04 16:41:10 +0900

    초기세팅

--- src/pages/SYha/Login/Login.js
+++ src/pages/SYha/Login/Login.js
@@ -1,7 +1,51 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import './Login.scss';
 const Login = () => {
-  return <h1>안녕하세요</h1>;
+  const navigate = useNavigate();
+
+  const goToMain = () => {
+    navigate('/main');
+  };
+
+  return (
+    <main className="body flex login_main">
+      <article className="mainBox">
+        <header className="login_mainlogo">
+          <h1 onClick={goToMain}>Westagram</h1>
+        </header>
+        <form className="flex form">
+          <input
+            className="pwAndId"
+            type="text"
+            // onkeyup='buttonactive()'
+            placeholder="전화번호, 사용자 이름 또는 이메일"
+            required
+            size="1"
+          />
+          <input
+            className="pwAndId"
+            type="password"
+            // onkeyup='buttonactive()'
+            placeholder="비밀번호"
+            required
+            size="1"
+          />
+
+          <Link className="link" to="/main">
+            <button className="loginbtn"> 로그인</button>
+          </Link>
+        </form>
+
+        <footer>
+          <a className="atag" href="/">
+            비밀번호를 잊으셨나요?
+          </a>
+        </footer>
+      </article>
+    </main>
+  );
 };
 
 export default Login;
