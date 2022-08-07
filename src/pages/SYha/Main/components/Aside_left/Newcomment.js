ddcc1521086e4783a8d468dfce85bb179ab1c268
seoyul <seoyul0203@gmail.com>
2022-08-07 09:05:35 +0900

    MODIFY:sider 리뷰 수정사항 반영_destructuring props

--- src/pages/SYha/Main/components/Aside_left/Newcomment.js
+++ src/pages/SYha/Main/components/Aside_left/Newcomment.js
@@ -2,11 +2,12 @@ import React from 'react';
 import './Newcomment.scss';
 
 const Newcomment = props => {
+  const { comment } = props;
   // 4. Newcomment 컴포넌트에서 props로 받아와서 사용
   return (
     <div className="comment_list">
       <strong>IamFriend </strong>
-      <p>{props.comment}</p>
+      <p>{comment}</p>
     </div>
   );
 };
