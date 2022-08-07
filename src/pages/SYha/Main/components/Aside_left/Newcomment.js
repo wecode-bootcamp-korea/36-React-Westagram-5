030ab5c309b1e16cb3198c546ac235073db4295c
seoyul <seoyul0203@gmail.com>
2022-08-07 16:07:31 +0900

    Add : Mission 4 - 댓글 컴포넌트화, Props로 데이터 전달 구현

--- src/pages/SYha/Main/components/Aside_left/Newcomment.js
+++ src/pages/SYha/Main/components/Aside_left/Newcomment.js
@@ -3,11 +3,18 @@ import './Newcomment.scss';
 
 const Newcomment = props => {
   const { comment } = props;
+  // const delete = () => {
+
+  // }
+
   // 4. Newcomment 컴포넌트에서 props로 받아와서 사용
   return (
     <div className="comment_list">
-      <strong>IamFriend </strong>
-      <p>{comment}</p>
+      <div className="idAndComment">
+        <strong>IamFriend </strong>
+        <p>{comment}</p>
+      </div>
+      <span onClick={() => alert('hi')}>X</span>
     </div>
   );
 };
