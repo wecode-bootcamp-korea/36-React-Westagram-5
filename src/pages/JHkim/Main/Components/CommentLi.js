82e94aad96ca7645d132a16497df699d07d0dae4
HYUKSKEE <gin280833@gmail.com>
2022-08-09 21:59:41 +0900

    [data 활용] Assignment 2 미션 리팩토링중

--- src/pages/JHkim/Main/Components/CommentLi.js
+++ src/pages/JHkim/Main/Components/CommentLi.js
@@ -1,27 +1,72 @@
+import { useState } from 'react';
 import LikeBtn from './LikeBtn';
 import './CommentLi.scss';
 
-const CommentLi = props => {
+const CommentLi = () => {
+  const [comments, setComment] = useState([]);
+  const users = 'Wecode Bootcamp ';
+
+  const handleUpload = event => {
+    event.preventDefault();
+    setComment(prevState => {
+      return [...prevState, input];
+    });
+    setInput('');
+  };
+
   const deleteLi = event => {
     const div = event.target.parentElement;
     const li = div.parentElement;
     li.remove();
   };
-  return props.comments.map((comment, idx) => {
-    return (
-      <li id={idx} key={idx} className="new-li">
-        <span>
-          <strong>{props.users}</strong>
-          {comment}
-        </span>
-        <div>
-          <button id="delete-button" onClick={deleteLi}>
-            X
+  const [input, setInput] = useState('');
+
+  const saveComment = event => {
+    setInput(event.target.value);
+  };
+
+  const disableUpload = event => {
+    event.preventDefault();
+  };
+  return (
+    <>
+      <ul id="comment-list">
+        {comments.map((comment, idx) => {
+          return (
+            <li id={idx} key={idx} className="new-li">
+              <span>
+                <strong>{users}</strong>
+                {comment}
+              </span>
+              <div>
+                <button id="delete-button" onClick={deleteLi}>
+                  X
+                </button>
+                <LikeBtn />
+              </div>
+            </li>
+          );
+        })}
+      </ul>
+      <div className="comment-input-wrap">
+        <form action="#" method="get">
+          <input
+            id="comment-input"
+            type="text"
+            value={input}
+            onChange={saveComment}
+            placeholder="댓글달기"
+          />
+          <button
+            type="submit"
+            id="upload-button"
+            onClick={input !== '' ? handleUpload : disableUpload}
+          >
+            게시
           </button>
-          <LikeBtn />
-        </div>
-      </li>
-    );
-  });
+        </form>
+      </div>
+    </>
+  );
 };
 export default CommentLi;
--- src/pages/JHkim/Main/Components/Feed.js
+++ src/pages/JHkim/Main/Components/Feed.js
@@ -1,95 +1,65 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import CommentLi from './CommentLi';
 import './Feed.scss';
 
 function Feed(props) {
-  const [comments, setComment] = useState([]);
-  const [input, setInput] = useState('');
-  const users = 'Wecode Bootcamp ';
+  const [feed, setFeed] = useState([]);
 
-  const saveComment = event => {
-    setInput(event.target.value);
-  };
+  useEffect(() => {
+    fetch('/data/HyukskeeFeed.json')
+      .then(response => response.json())
+      .then(result => setFeed(result));
+  }, []);
 
-  const handleUpload = event => {
-    event.preventDefault();
-    setComment(prevState => {
-      return [...prevState, input];
-    });
-    setInput('');
-  };
-
-  const disableUpload = event => {
-    event.preventDefault();
-  };
-
-  return (
-    <div className="feed">
-      <article>
-        <div className="article-head">
-          <h2>
-            <img src="images/JHKim/운명 트로트 2.jpg" alt="This is profile" />
-            <p>HYUKSKEE</p>
-          </h2>
-          <i className="fas fa-thin fa-ellipsis" />
-        </div>
-        <div className="article-content">
-          <img
-            className="article-main-img"
-            src="images/JHKim/야나두.gif"
-            alt="This is article"
-          />
-          <div className="article-btn-img">
-            <div className="left-side">
+  return feed.map(feed => {
+    return (
+      <div key={feed.ID} className="feed">
+        <article>
+          <div className="article-head">
+            <h2>
+              <img src={feed.HeaderImg} alt="This is profile" />
+              <p>{feed.userId}</p>
+            </h2>
+            <i className="fas fa-thin fa-ellipsis" />
+          </div>
+          <div className="article-content">
+            <img
+              className="article-main-img"
+              src={feed.TitleImg}
+              alt="This is article"
+            />
+            <div className="article-btn-img">
+              <div className="left-side">
+                <img
+                  className="btn-img"
+                  src="images/JHKim/heart.png"
+                  alt="This is heart icon"
+                />
+                <img
+                  className="btn-img"
+                  src="images/JHKim/chat.png"
+                  alt="This is comment icon"
+                />
+                <img
+                  className="btn-img"
+                  src="images/JHKim/export.png"
+                  alt="This is share icon"
+                />
+              </div>
               <img
                 className="btn-img"
-                src="images/JHKim/heart.png"
-                alt="This is heart icon"
-              />
-              <img
-                className="btn-img"
-                src="images/JHKim/chat.png"
-                alt="This is comment icon"
-              />
-              <img
-                className="btn-img"
-                src="images/JHKim/export.png"
-                alt="This is share icon"
+                src="images/JHKim/tag.png"
+                alt="This is book mark icon"
               />
             </div>
-            <img
-              className="btn-img"
-              src="images/JHKim/tag.png"
-              alt="This is book mark icon"
-            />
           </div>
-        </div>
-        <div className="article-comment">
-          <ul id="comment-list">
-            <CommentLi comments={comments} users={users} />
-          </ul>
-          <div className="comment-input-wrap">
-            <form action="#" method="get">
-              <input
-                id="comment-input"
-                type="text"
-                value={input}
-                onChange={saveComment}
-                placeholder="댓글달기"
-              />
-              <button
-                type="submit"
-                id="upload-button"
-                onClick={input !== '' ? handleUpload : disableUpload}
-              >
-                게시
-              </button>
-            </form>
+          <div className="article-comment">
+            <CommentLi />
           </div>
-        </div>
-      </article>
-    </div>
-  );
+        </article>
+      </div>
+    );
+  });
 }
 
 export default Feed;
