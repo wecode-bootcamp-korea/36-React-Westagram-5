47158bb7d40d39d912c95d84713dc8901ecd6429
함정석 <hahmjeongseok@hamjeongseog-ui-MacBookAir.local>
2022-08-11 13:51:18 +0900

    추천부분 수정

--- src/components/MainRight/MainRight.js
+++ src/components/MainRight/MainRight.js
@@ -5,14 +5,58 @@ const Main_right = () => {
   return (
     <div className="main_right">
       <div className="recommendList">
-        <img
-          src="images/JSham/story_profile.jpg"
-          className="recommendSize"
-          alt="profile"
-        />
-        <span className="recommendId" alt="profile">
-          clean_0828
-        </span>
+        <ul>
+          <li>
+            <img
+              src="images/JSham/story_profile.jpg"
+              className="recommendSize"
+              alt="profile"
+            />
+            <span className="recommendId" alt="profile">
+              clean_0828
+            </span>
+          </li>
+          <li>
+            <img
+              src="images/JSham/story_profile2.jpg"
+              className="recommendSize"
+              alt="profile"
+            />
+            <span className="recommendId" alt="profile">
+              taeyon_ss
+            </span>
+          </li>
+          <li>
+            <img
+              src="images/JSham/story_profile3.jpg"
+              className="recommendSize"
+              alt="profile"
+            />
+            <span className="recommendId" alt="profile">
+              dlwlrma
+            </span>
+          </li>
+          <li>
+            <img
+              src="images/JSham/story_profile4.jpg"
+              className="recommendSize"
+              alt="profile"
+            />
+            <span className="recommendId" alt="profile">
+              jennierubyjane
+            </span>
+          </li>
+          <li>
+            <img
+              src="images/JSham/story_profile5.jpg"
+              className="recommendSize"
+              alt="profile"
+            />
+            <span className="recommendId" alt="profile">
+              yuqisong.923
+            </span>
+          </li>
+        </ul>
       </div>
     </div>
   );
